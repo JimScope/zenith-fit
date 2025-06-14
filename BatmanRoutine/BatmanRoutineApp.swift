a56055835113797938522e53b5fc9ec1f7693f26
Jimmy Angel Pérez Díaz <jimmy@factory.generalsoftwareinc.com>
2025-06-14 19:04:35 -0400

    Initial Commit

---
+++ BatmanRoutine/BatmanRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatmanRoutineApp.swift
+//  BatmanRoutine
+//
+//  Created by Jimmy Angel Pérez Díaz on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BatmanRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatmanRoutine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BatmanRoutine
+//
+//  Created by Jimmy Angel Pérez Díaz on 6/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
